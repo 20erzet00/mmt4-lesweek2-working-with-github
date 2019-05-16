5cf4e9ca9c7ebc839ebe78fc264aa5891fe528b3
20erzet00 <50697460+20erzet00@users.noreply.github.com>
2019-05-16 11:31:09 +0200

    create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// 1 selector
+// 2 funtie
+// 3 parameter
+$("#button").click(function() {
+    $("#button").css("color", "#fff");
+    //$("body").fadeOut(10000);
+    $("#button").animate({"margin-right": "600px"}, 100);
+});
